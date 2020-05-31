3adaa931982923cbeaeb4aed30904c45fe87b624
杨有旺 <616505453@qq.com>
2020-05-31 18:24:35 +0800

    完善相关测试

--- src/test/java/com/sdgdw/sign/AbstractSignTest.java
+++ src/test/java/com/sdgdw/sign/AbstractSignTest.java
@@ -2,10 +2,14 @@ package com.sdgdw.sign;
 
 import com.sdgdw.sign.client.SignClient;
 import com.sdgdw.sign.client.support.DefaultSignClient;
+import com.sdgdw.sign.module.request.TokenRequest;
+import com.sdgdw.sign.module.response.TokenResponse;
 import org.junit.Before;
+import org.junit.Test;
 
 /**
- * 基础的测试类，抽取公共方法
+ * 基础的测试类，实例化请求客户端
+ *
  * @author modificial
  * @description
  * @since 2020/5/19
@@ -19,8 +23,18 @@ public abstract class AbstractSignTest {
      */
     @Before
     public void init() {
-        signClient = new DefaultSignClient("http://localhost:8081/v1/","710511183694286848","UfMTWTuvONXkCzmqtm");
+        signClient = new DefaultSignClient("api网关地址", "您的appid", "您的appsecret");
     }
 
-
+    /**
+     * 获取签名值
+     */
+    @Test
+    public void getToken() {
+        TokenRequest tokenRequest = new TokenRequest("appid", "appsecret");
+        TokenResponse response = signClient.execute(tokenRequest);
+        if (response.isSuccess()) {
+            System.out.println("获取token成功，响应值为" + response.getData());
+        }
+    }
 }

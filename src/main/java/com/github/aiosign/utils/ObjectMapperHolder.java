be51735c7b7e43feffd69fdc3a85d2d65399b7f4
modificial <modificial@aliyun.com>
2020-08-17 14:06:21 +0800

    修改因json参数过多导致ObjectMapper反序列化失败

--- src/main/java/com/github/aiosign/utils/ObjectMapperHolder.java
+++ src/main/java/com/github/aiosign/utils/ObjectMapperHolder.java
@@ -1,11 +1,14 @@
 package com.github.aiosign.utils;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.github.aiosign.module.request.BarCodeRequest;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -39,6 +42,7 @@ public enum ObjectMapperHolder {
         timeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_PATTERN)));
         timeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DATE_PATTERN)));
         objectMapper.registerModule(timeModule);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
     }
 
     /**

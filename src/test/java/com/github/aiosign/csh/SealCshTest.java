7ea6dc77e4a113a28cd06cae97dbf12213e1aaa0
zhudunfeng <2101580047@qq.com>
2023-06-19 15:59:49 +0800

    feat:印章申领添加AES

--- src/test/java/com/github/aiosign/csh/SealCshTest.java
+++ src/test/java/com/github/aiosign/csh/SealCshTest.java
@@ -6,6 +6,7 @@ import com.github.aiosign.enums.ContentType;
 import com.github.aiosign.enums.HttpMethod;
 import com.github.aiosign.module.request.CommonRequest;
 import com.github.aiosign.module.response.CommonResponse;
+import com.github.aiosign.utils.AESUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
@@ -34,6 +35,35 @@ public class SealCshTest extends AbstractSignTest {
 
         CommonRequest request = new CommonRequest();
         request.setApiUri("/v1/seal/getSealInfos"); // 请求Api地址
+        request.setNeedToken(true);// 是否需要token
+        request.setContentType(ContentType.JSON);// 请求头类型
+        request.setMethod(HttpMethod.POST);// 请求方法
+        request.setRequestBody(requestBody);// 请求体
+
+        CommonResponse execute = signClient.execute(request);
+        log.info("响应状态：{}", execute.getResultCode());
+        log.info("响应信息：{}", execute.getResultMessage());
+        log.info("响应数据：{}", JSON.toJSONString(execute.getData()));
+    }
+
+    /**
+     * 电子印章申领
+     */
+    @Test
+    public void applySeal() {
+        // AESKey为密钥，对接时下发
+        final String AESKey = "U6rmwyUKmPU7zCML";
+        HashMap<String, String> requestBody = new HashMap<>(2);
+        String areaCode="450100";
+        //企业名称（密）
+        requestBody.put("user_name", AESUtils.encrypt(AESKey,"测试山东国盾网"));
+        //企业社会信用代码（密）
+        requestBody.put("credit_code", AESUtils.encrypt(AESKey, "235122212233212"));
+        //地区编码
+        requestBody.put("area_code",areaCode);
+
+        CommonRequest request = new CommonRequest();
+        request.setApiUri("/v1/seal/apply-seal-en"); // 请求Api地址
         request.setNeedToken(true);// 是否需要token
         request.setContentType(ContentType.JSON);// 请求头类型
         request.setMethod(HttpMethod.POST);// 请求方法
